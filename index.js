77807c986889fae75d23b3640642f5ff37843374
Tawkir711 <mdtawkirk711@gmail.com>
2023-11-27 00:41:45 +0600

    added delete method

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const port = process.env.PORT || 5000;
 
@@ -36,6 +36,13 @@ async function run() {
     app.post('/addCamp', async (req, res) => {
       const item = req.body;
       const result = await addCampCollection.insertOne(item);
+      res.send(result);
+    })
+
+    app.delete('/addCamp/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await addCampCollection.deleteOne(query);
       res.send(result);
     })
 
