22875ec69870bb8cedb97d4c63dfeb15d54bc6ad
Tawkir711 <mdtawkirk711@gmail.com>
2023-11-29 10:37:10 +0600

    install and added jsonwebtoken

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const jwt = require('jsonwebtoken');
 require('dotenv').config()
 const port = process.env.PORT || 5000;
 
@@ -30,6 +31,13 @@ async function run() {
     const userCollection = client.db('MedicalCamp').collection('users');
     const joinCampCollection = client.db('MedicalCamp').collection('joinCamp');
 
+
+    // jwt related api item 
+    app.post('/jwt', async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN, { expiresIn: '1h' })
+      res.send({ token });
+    })
 
 
     // user related item
