de5e738bf7f1749d8d7cd6a81b4bee3733012527
Tawkir711 <mdtawkirk711@gmail.com>
2023-11-29 11:28:03 +0600

    added verify token and middleware

--- index.js
+++ index.js
@@ -40,9 +40,26 @@ async function run() {
     })
 
 
+    // middleware 
+    const verifyToken = (req, res, next) => {
+      console.log('inside verify token', req.headers.authorization);
+      if (!req.headers.authorization) {
+        return res.status(401).send({ message: 'unauthorized access' })
+      }
+      const token = req.headers.authorization.split(' ')[1];
+      jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({ message: 'unauthorized access' })
+        }
+        req.decoded = decoded;
+        next();
+      })
+    }
+
+
     // user related item
 
-    app.get('/users', async (req, res) => {
+    app.get('/users', verifyToken, async (req, res) => {
       const result = await userCollection.find().toArray();
       res.send(result);
     })
