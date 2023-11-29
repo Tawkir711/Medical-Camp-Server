2e69da30722d20da221fb3187369dda536b79e4b
Tawkir711 <mdtawkirk711@gmail.com>
2023-11-29 10:03:07 +0600

    added delete method

--- index.js
+++ index.js
@@ -50,6 +50,13 @@ async function run() {
       res.send(result);
     })
 
+    app.delete('/users/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+    })
+
 
     // join camp related api item
     app.get('/joinCamp', async (req, res) => {
